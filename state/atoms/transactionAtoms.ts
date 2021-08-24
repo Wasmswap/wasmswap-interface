1d5985f2158ef856ad6a58b2e8d2bbf1f004b3c8
ben <ben2x4@tutanota.com>
2021-08-23 22:48:38 -0700

    button to approve token allowance

--- state/atoms/transactionAtoms.ts
+++ state/atoms/transactionAtoms.ts
@@ -1,7 +1,9 @@
 import { atom } from 'recoil'
 
+export type State = 'IDLE' | 'APPROVING_ALLOWANCE' | 'ALLOWANCE_APPROVED' | 'EXECUTING_SWAP' 
+
 export const transactionStatusState = atom<
-  'IDLE' | 'SUCCESS' | 'ERROR' | 'FETCHING'
+  State
 >({
   key: 'transactionState',
   default: 'IDLE',

1baed27b1389184bdc692ba5e465c9d2f918db5d
ben <ben2x4@tutanota.com>
2021-08-30 12:21:26 -0700

    typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -274,7 +274,7 @@ export default function Home() {
         </section>
       </SwapFormFrame>
       <Disclaimer delayMs={3000}>
-        Wasmswap is currently in beta and operating on the Juno testnet. Keplr connect to a ledger is currently unsupported.
+        Wasmswap is currently in beta and operating on the Juno testnet. Keplr connected to a ledger is currently unsupported.
       </Disclaimer>
     </div>
   )

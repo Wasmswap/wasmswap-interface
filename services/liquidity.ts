2c8bdfe394e25fbf3a7fd414cdfe487413ec73fc
ben2x4 <86395884+ben2x4@users.noreply.github.com>
2021-09-20 12:18:12 -0700

    Utils for managing liquidity (#22)
    * Utils for managing liquidity
    
    * syntax error

---
+++ services/liquidity.ts
@@ -0,0 +1,115 @@
+import {
+  SigningCosmWasmClient,
+  CosmWasmClient,
+  MsgExecuteContractEncodeObject,
+} from '@cosmjs/cosmwasm-stargate'
+import { MsgExecuteContract } from '@cosmjs/cosmwasm-stargate/build/codec/cosmwasm/wasm/v1beta1/tx'
+import { toUtf8 } from '@cosmjs/encoding'
+import { coin, StdFee } from '@cosmjs/launchpad'
+import { BroadcastTxResponse } from '@cosmjs/stargate'
+
+export type AddLiquidityInput = {
+  nativeAmount: number
+  nativeDenom: string
+  maxToken: number
+  minLiquidity: number
+  senderAddress: string
+  swapAddress: string
+  tokenAddress: string
+  client: SigningCosmWasmClient
+}
+
+export const addLiquidity = async (
+  input: AddLiquidityInput
+): Promise<BroadcastTxResponse> => {
+  let msg1 = {
+    increase_allowance: {
+      amount: `${input.maxToken}`,
+      spender: `${input.swapAddress}`,
+    },
+  }
+  const executeContractMsg1: MsgExecuteContractEncodeObject = {
+    typeUrl: '/cosmwasm.wasm.v1beta1.MsgExecuteContract',
+    value: MsgExecuteContract.fromPartial({
+      sender: input.senderAddress,
+      contract: input.tokenAddress,
+      msg: toUtf8(JSON.stringify(msg1)),
+      funds: [],
+    }),
+  }
+  let msg2 = {
+    add_liquidity: {
+      max_token: `${input.maxToken}`,
+      min_liquidity: `${input.minLiquidity}`,
+    },
+  }
+  const executeContractMsg2: MsgExecuteContractEncodeObject = {
+    typeUrl: '/cosmwasm.wasm.v1beta1.MsgExecuteContract',
+    value: MsgExecuteContract.fromPartial({
+      sender: input.senderAddress,
+      contract: input.swapAddress,
+      msg: toUtf8(JSON.stringify(msg2)),
+      funds: [coin(input.nativeAmount, input.nativeDenom)],
+    }),
+  }
+  const fee: StdFee = {
+    amount: input.client.fees.exec.amount,
+    gas: (+input.client.fees.exec.gas * 2).toString(),
+  }
+  const executeAddLiquidity = await input.client.signAndBroadcast(
+    input.senderAddress,
+    [executeContractMsg1, executeContractMsg2],
+    fee
+  )
+  console.log(executeAddLiquidity)
+  return executeAddLiquidity
+}
+
+export type RemoveLiquidityInput = {
+  amount: number
+  minNative: number
+  minToken: number
+  senderAddress: string
+  swapAddress: string
+  tokenAddress: string
+  client: SigningCosmWasmClient
+}
+
+export const removeLiquidity = async (input: RemoveLiquidityInput) => {
+  const msg = {
+    remove_liquidity: {
+      amount: `${input.amount}`,
+      min_native: `${input.minNative}`,
+      min_token: `${input.minToken}`,
+    },
+  }
+  const execute = await input.client.execute(
+    input.senderAddress,
+    input.swapAddress,
+    msg,
+    undefined,
+    []
+  )
+  return execute
+}
+
+export type GetLiquidityBalanceInput = {
+  address: string
+  swapAddress: string
+  rpcEndpoint: string
+}
+
+export const getLiquidityBalance = async (input: GetLiquidityBalanceInput) => {
+  try {
+    const client = await CosmWasmClient.connect(input.rpcEndpoint)
+    const query = await client.queryContractSmart(input.swapAddress, {
+      balance: {
+        address: input.address,
+      },
+    })
+    console.log(query)
+    return query.balance
+  } catch (e) {
+    console.log(e)
+  }
+}

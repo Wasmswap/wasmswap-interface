91143097397dafd9382bf964c5d10527311a9b77
Nikita Trifan <nikitatrifan@Nikitas-MBP.hsd1.ca.comcast.net>
2021-09-28 21:06:37 -0700

    add some more logging for debugging

--- hooks/useLiquidity.tsx
+++ hooks/useLiquidity.tsx
@@ -4,24 +4,40 @@ import { getLiquidityBalance } from '../services/liquidity'
 
 export const useLiquidity = ({ tokenName, address, swapAddress }) => {
   const {
-    data: { native_reserve, lp_token_supply } = {},
+    data: { native_reserve, lp_token_supply, ...etc } = {},
     isLoading: fetchingTotalLiquidity,
   } = useQuery(`totalLiquidity/${tokenName}`, () =>
     getSwapInfo(swapAddress, process.env.NEXT_PUBLIC_CHAIN_RPC_ENDPOINT)
   )
 
+  console.log('totalLiquidityQuery', {
+    native_reserve,
+    lp_token_supply,
+    ...etc,
+  })
+
   const { data: myLiquidity, isLoading: fetchingMyLiquidity } = useQuery(
     [`myLiquidity/${tokenName}`, address, native_reserve, lp_token_supply],
     async () => {
-      const { balance } = await getLiquidityBalance({
+      const balance = await getLiquidityBalance({
         address: address,
         swapAddress: swapAddress,
         rpcEndpoint: process.env.NEXT_PUBLIC_CHAIN_RPC_ENDPOINT,
       })
 
-      return (balance / Number(lp_token_supply)) * Number(native_reserve) * 2
+      console.log({
+        balance,
+        lp_token_supply,
+        native_reserve,
+      })
+
+      return (
+        (Number(balance) / Number(lp_token_supply)) * Number(native_reserve) * 2
+      )
     }
   )
+
+  console.log(myLiquidity)
 
   return {
     totalLiquidity: native_reserve ? (Number(native_reserve) * 2) / 1000000 : 0,

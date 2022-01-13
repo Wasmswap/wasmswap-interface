d436d49c0ffd41cb38a6a4461b5804829c86c4b2
Andrea Di Michele <dimiandre@gmail.com>
2022-01-13 15:03:59 -0500

    Update Logo.tsx

--- icons/Logo.tsx
+++ icons/Logo.tsx
@@ -3,13 +3,7 @@ import { SVGProps } from 'react'
 export const Logo = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg
-       xmlns:osb="http://www.openswatchbook.org/uri/2009/osb"
-       xmlns:dc="http://purl.org/dc/elements/1.1/"
-       xmlns:cc="http://creativecommons.org/ns#"
-       xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-       xmlns:svg="http://www.w3.org/2000/svg"
        xmlns="http://www.w3.org/2000/svg"
-       xmlns:xlink="http://www.w3.org/1999/xlink"
        id="svg7302"
        version="1.1"
        viewBox="0 0 396.875 396.87501"

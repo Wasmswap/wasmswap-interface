a4425ee90f658b8dc902c2a230a5a28e0a72b066
sashimi36 <93623541+sashimi36@users.noreply.github.com>
2022-08-19 10:33:58 -0700

    Protect `useGetTokenDollarValueQuery` in case if token to token price is not defined

--- queries/useGetTokenDollarValueQuery.ts
+++ queries/useGetTokenDollarValueQuery.ts
@@ -1,13 +1,15 @@
-/*
- * takes base token price, fetches the ratio of the token provided vs the base token
- * and calculates the dollar value of the provided token
- * */
+import { protectAgainstNaN } from 'junoblocks'
+
 import { useCosmWasmClient } from '../hooks/useCosmWasmClient'
 import { useTokenDollarValue } from '../hooks/useTokenDollarValue'
 import { useBaseTokenInfo } from '../hooks/useTokenInfo'
 import { tokenToTokenPriceQueryWithPools } from './tokenToTokenPriceQuery'
 import { useGetQueryMatchingPoolForSwap } from './useQueryMatchingPoolForSwap'
 
+/*
+ * takes base token price, fetches the ratio of the token provided vs the base token
+ * and calculates the dollar value of the provided token
+ * */
 export const useGetTokenDollarValueQuery = () => {
   const tokenA = useBaseTokenInfo()
   const client = useCosmWasmClient()
@@ -22,17 +24,19 @@ export const useGetTokenDollarValueQuery = () => {
     async function getTokenDollarValue({ tokenInfo, tokenAmountInDenom }) {
       if (!tokenAmountInDenom) return 0
 
-      const { price: priceForOneToken } = await tokenToTokenPriceQueryWithPools(
-        {
+      const priceForOneToken = (
+        await tokenToTokenPriceQueryWithPools({
           matchingPools: getMatchingPoolForSwap({ tokenA, tokenB: tokenInfo }),
           tokenA,
           tokenB: tokenInfo,
           client,
           amount: 1,
-        }
-      )
+        })
+      )?.price
 
-      return (tokenAmountInDenom / priceForOneToken) * tokenADollarPrice
+      return protectAgainstNaN(
+        (tokenAmountInDenom / priceForOneToken) * tokenADollarPrice
+      )
     },
     Boolean(
       tokenA && client && !fetchingDollarPrice && !isLoadingPoolForSwapMatcher

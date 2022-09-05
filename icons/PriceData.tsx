b400510b28591ccc5a7a9660f0fff200e126365a
Bao <baomai@berkeley.edu>
2022-09-05 13:52:21 -0700

    fixed price icon

--- icons/PriceData.tsx
+++ icons/PriceData.tsx
@@ -3,7 +3,7 @@ export const PriceData = () => (
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    fill="none"
+    fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
   >
     <mask
@@ -16,11 +16,11 @@ export const PriceData = () => (
     >
       <rect width="24" height="24" fill="currentColor" />
     </mask>
-    <g mask="url(#mask0_1461_10624)">
+    <g>
       <path
         d="M3.5 19.4996V7.99961L7.125 10.6996L12.1 3.72461L16.825 7.49961H20.5V19.4996H3.5ZM8 16.9996L12 11.4996L19 16.9496V8.99961H16.3L12.4 5.87461L7.45 12.8246L5 10.9996V14.5996L8 16.9996Z"
         fill="currentColor"
-        fill-opacity="0.7"
+        fill-opacity="1"
       />
     </g>
   </svg>

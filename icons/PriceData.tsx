2bbe22d87806555e8c71f0e976ec34d3539de511
rawasabi <102771198+rawasabi@users.noreply.github.com>
2022-09-12 12:10:02 -0700

    fix fillOpacity

--- icons/PriceData.tsx
+++ icons/PriceData.tsx
@@ -10,7 +10,7 @@ export const PriceData = () => (
       <path
         d="M3.5 19.4996V7.99961L7.125 10.6996L12.1 3.72461L16.825 7.49961H20.5V19.4996H3.5ZM8 16.9996L12 11.4996L19 16.9496V8.99961H16.3L12.4 5.87461L7.45 12.8246L5 10.9996V14.5996L8 16.9996Z"
         fill="currentColor"
-        fill-opacity="1"
+        fillOpacity="1"
       />
     </g>
   </svg>

1546a0038d88b91d8d1e526b2488d6f2d8abf38f
Bao <baomai@berkeley.edu>
2022-09-20 16:14:00 -0700

    transaction fee

--- features/swap/components/TransactionTips.tsx
+++ features/swap/components/TransactionTips.tsx
@@ -113,7 +113,7 @@ export const TransactionTips = ({
         )}
       </StyledDivForRateWrapper>
 
-      <Text variant="legend">${formattedDollarValue}</Text>
+      <Text variant="legend">{isShowing && `${formattedDollarValue}`}</Text>
     </StyledDivForWrapper>
   )
 }

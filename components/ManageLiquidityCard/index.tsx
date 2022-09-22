891e82aafee8e9e257d223a5df88a212a26db249
Bao <baomai@berkeley.edu>
2022-09-22 14:03:24 -0700

    Most of liquidity card is migrated

--- components/ManageLiquidityCard/index.tsx
+++ components/ManageLiquidityCard/index.tsx
@@ -1,1 +1,92 @@
-export const ManageLiquidityCard = () => <div>Hello world</div>
+import { UnderlyingAssetRow } from '../../features/liquidity/components/UnderlyingAssetRow'
+import {
+  Button,
+  Card,
+  CardContent,
+  Column,
+  Divider,
+  dollarValueFormatterWithDecimals,
+  Inline,
+  Text,
+} from 'junoblocks'
+import { FC } from 'react'
+
+export type ManageLiquidityCardProps = {
+  liquidityDollarValue?: number
+  availableToBondDollarValue?: number
+  supportsIncentives: boolean
+  didProvideLiquidity: boolean
+  onClick: () => void
+}
+
+export const ManageLiquidityCard: FC<ManageLiquidityCardProps> = ({
+  liquidityDollarValue,
+  availableToBondDollarValue,
+  supportsIncentives,
+  didProvideLiquidity,
+  onClick,
+}) => {
+  return (
+    <Card
+      tabIndex={-1}
+      role="button"
+      variant={
+        didProvideLiquidity ? 'primary' : 'secondary'
+      }
+      onClick={onClick}
+    >
+      <CardContent>
+        <Text variant="legend" color="body" css={{ padding: '$16 0 $6' }}>
+          Holdings
+        </Text>
+        <Text variant="hero">${liquidityDollarValue}</Text>
+        <Text variant="link" color="brand" css={{ paddingTop: '$2' }}>
+          $
+          {dollarValueFormatterWithDecimals(availableToBondDollarValue, {
+            includeCommaSeparation: true,
+          })}{' '}
+          available
+          {supportsIncentives ? ' to bond' : ''}
+        </Text>
+      </CardContent>
+      <Divider offsetTop="$14" offsetBottom="$12" />
+      <CardContent>
+        <Text variant="legend" color="secondary" css={{ paddingBottom: '$12' }}>
+          Underlying assets
+        </Text>
+        <Column gap={6} css={{ paddingBottom: '$16' }}>
+          {/* <UnderlyingAssetRow tokenInfo={tokenA} tokenAmount={tokenAReserve} />
+          <UnderlyingAssetRow tokenInfo={tokenB} tokenAmount={tokenBReserve} /> */}
+        </Column>
+        <Inline css={{ paddingBottom: '$12' }}>
+          {didProvideLiquidity && (
+            <Button
+              variant="secondary"
+              size="large"
+              css={{ width: '100%' }}
+              onClick={(e) => {
+                e.stopPropagation()
+                onClick?.()
+              }}
+            >
+              Manage Liquidity
+            </Button>
+          )}
+          {!didProvideLiquidity && (
+            <Button
+              variant="primary"
+              size="large"
+              css={{ width: '100%' }}
+              onClick={(e) => {
+                e.stopPropagation()
+                onClick?.()
+              }}
+            >
+              Add Liquidity
+            </Button>
+          )}
+        </Inline>
+      </CardContent>
+    </Card>
+  )
+}

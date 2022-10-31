a33f64b0bfdae9263cdcdd6113c4ff1422438747
rawasabi <102771198+rawasabi@users.noreply.github.com>
2022-10-31 14:03:55 -0700

    add optional children to PageHeader component

--- components/Layout/PageHeader.tsx
+++ components/Layout/PageHeader.tsx
@@ -1,10 +1,11 @@
 import { Text, useMedia } from 'junoblocks'
+import { Row } from '../../components'
 import Head from 'next/head'
 import React from 'react'
 
 import { APP_NAME } from '../../util/constants'
 
-export const PageHeader = ({ title, subtitle }) => {
+export const PageHeader = ({ title, subtitle, children = undefined }) => {
   const isSmall = useMedia('sm')
 
   return (
@@ -14,9 +15,15 @@ export const PageHeader = ({ title, subtitle }) => {
           {APP_NAME} — {title}
         </title>
       </Head>
-      <Text variant="header" css={{ padding: isSmall ? '$15 0 $6' : '$10 0' }}>
-        {title}
-      </Text>
+      <Row>
+        <Text
+          variant="header"
+          css={{ padding: isSmall ? '$15 0 $6' : '$10 0' }}
+        >
+          {title}
+        </Text>
+        {children}
+      </Row>
       <Text variant="body" css={{ paddingBottom: isSmall ? '$12' : '$16' }}>
         {subtitle}
       </Text>

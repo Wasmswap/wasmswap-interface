71b2a1d6fd253f985ba6094a18e07ba663baf263
rawasabi <102771198+rawasabi@users.noreply.github.com>
2022-10-31 14:02:48 -0700

    add Row component

---
+++ components/Row.tsx
@@ -0,0 +1,8 @@
+import { styled } from 'junoblocks'
+
+export const Row = styled('div', {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})

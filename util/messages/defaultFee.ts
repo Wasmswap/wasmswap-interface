138da17dff32eb3ea26721e5541e87194c1c33d2
rawasabi <102771198+rawasabi@users.noreply.github.com>
2022-11-01 16:44:44 -0700

    update defaultFee to 500_000

--- util/messages/defaultFee.ts
+++ util/messages/defaultFee.ts
@@ -3,4 +3,4 @@ import { GasPrice, calculateFee } from '@cosmjs/stargate'
 
 export const DEFAULT_FEE = !__DISABLE_GAS_AUTO__
   ? 'auto'
-  : calculateFee(80_000, GasPrice.fromString(GAS_PRICE))
+  : calculateFee(500_000, GasPrice.fromString(GAS_PRICE))

c51bf0dcff151f4691b27fa266bf5abeeca09cc9
rawasabi <102771198+rawasabi@users.noreply.github.com>
2022-11-01 16:25:54 -0700

    fix defaultFee constants import

--- util/messages/defaultFee.ts
+++ util/messages/defaultFee.ts
@@ -1,4 +1,4 @@
-import { __DISABLE_GAS_AUTO__, GAS_PRICE } from './constants'
+import { __DISABLE_GAS_AUTO__, GAS_PRICE } from '../constants'
 import { GasPrice, calculateFee } from '@cosmjs/stargate'
 
 export const DEFAULT_FEE = !__DISABLE_GAS_AUTO__ ? 'auto' : calculateFee(80_000,GasPrice.fromString(GAS_PRICE))

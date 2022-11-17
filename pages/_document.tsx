21a73897113ae6f0bafa1f5041781a55a5852139
rawasabi <102771198+rawasabi@users.noreply.github.com>
2022-11-16 16:21:59 -0800

    cleanup _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,6 @@
-import Document, {
-  DocumentContext,
-  DocumentInitialProps,
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx)
-
-    return initialProps
-  }
-
   render() {
     return (
       <Html>
